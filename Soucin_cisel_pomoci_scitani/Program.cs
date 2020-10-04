940968f76d21cba89a448ceb130294c84007d895
Windows <Windows@DESKTOP-QCE28GP>
2020-10-04 20:25:57 +0200

    Přidejte soubory projektu.

---
+++ Soucin_cisel_pomoci_scitani/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+
+namespace Soucin_cisel_pomoci_scitani
+{   
+    class Program
+    {
+        static int CountByMultiplication(int cislo1, int cislo2);
+        {
+        int result = 0;
+        for (int i = 0; i< = cislo2; i++);
+            {
+            result = result + cislo1;
+            }
+        return result;
+        }
+        static void Main(string [] args)
+        {
+            Console.WriteLine("Zadej cislo 1");
+    string input1 = Console.ReadLine();
+    Console.WriteLine("Zadej cislo 2 ");
+    string input2 = Console.ReadLine();
+    input cislo1 = Convert.ToInt32(input1);
+    input cislo2 = Convert.ToInt32(input2);
+
+    Console.WriteLine(cislo1 + "*" + cislo2 + "=" + Console.WriteLine(num1 + "*" + num2 + "=" + CountByMultiplication(cislo1, cislo2));
+}
+    }
+}
+
+
+
+
+
+
